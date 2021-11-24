f6db866b7f00068ae13928849890c330b54e49c3
Nuri Engin <nuri@engin.web.tr>
2021-11-24 17:57:48 +0300

    ✨ : Primary keys implemented

--- install-scripts/all-structure.sql
+++ install-scripts/all-structure.sql
@@ -91,46 +91,6 @@ COPY public.store (store_id, manager_staff_id, address_id, last_update) FROM std
 \.
 COPY public.store (store_id, manager_staff_id, address_id, last_update) FROM '$$PATH$$/3081.dat';
 
--- Name: actor_actor_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.actor_actor_id_seq', 200, true);
-
--- Name: address_address_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.address_address_id_seq', 605, true);
-
-
--- Name: category_category_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.category_category_id_seq', 16, true);
-
--- Name: city_city_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.city_city_id_seq', 600, true);
-
--- Name: country_country_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.country_country_id_seq', 109, true);
-
--- Name: customer_customer_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.customer_customer_id_seq', 599, true);
-
--- Name: film_film_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.film_film_id_seq', 1000, true);
-
--- Name: inventory_inventory_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.inventory_inventory_id_seq', 4581, true);
-
--- Name: language_language_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.language_language_id_seq', 6, true);
-
--- Name: payment_payment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.payment_payment_id_seq', 32098, true);
-
--- Name: rental_rental_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.rental_rental_id_seq', 16049, true);
-
--- Name: staff_staff_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.staff_staff_id_seq', 2, true);
-
--- Name: store_store_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
-SELECT pg_catalog.setval('public.store_store_id_seq', 2, true);
-
 -- Name: actor actor_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 ALTER TABLE ONLY public.actor
     ADD CONSTRAINT actor_pkey PRIMARY KEY (actor_id);
